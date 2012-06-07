9cc01a28c3c26de63d61e8ddb629534628d886a0
Grant Goodale <ggoodale@gmail.com>
2012-06-07 09:21:15 -0700

    Update middleware and config to current vagrant API; rev version

--- lib/vagrant-aws/config.rb
+++ lib/vagrant-aws/config.rb
@@ -2,7 +2,6 @@ module VagrantAWS
 	# A configuration class to configure defaults which are used for
   # the `vagrant-aws` plugin.
   class Config < Vagrant::Config::Base
-    configures :aws
    	
 		attr_accessor :key_name
 		attr_writer   :private_key_path
@@ -35,4 +34,6 @@ module VagrantAWS
 			errors.add(I18n.t("vagrant.config.ssh.private_key_missing", :path => private_key_path)) if private_key_path && !File.exists?(private_key_path)
 		end
 	end
+
+	Vagrant.config_keys.register(:aws) { Config }
 end	
