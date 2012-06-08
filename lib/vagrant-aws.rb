cbe021b6c99f3b741bbec54b495278d1677797e7
Grant Goodale <ggoodale@gmail.com>
2012-06-08 16:39:43 -0700

    Stop writing tmpfiles in the vagrant gem's source tree

--- lib/vagrant-aws.rb
+++ lib/vagrant-aws.rb
@@ -10,3 +10,12 @@ require 'vagrant-aws/box'
 
 # Add our custom translations to the load path
 I18n.load_path << File.expand_path("../../locales/en.yml", __FILE__)
+
+module VagrantAWS
+  # The source root is the path to the root directory of
+  # the Vagrant gem.
+  def self.source_root
+    @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
+  end
+end
+
